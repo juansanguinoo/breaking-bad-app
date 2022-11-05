13e90f34840f036478ad0379a93dcd1e7f0c86c3
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2022-11-05 12:37:31 -0500

    feat: breaking-bad-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import Phrase from './components/Phrase';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size 0.8s ease;
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
 
 function App() {
+  const [phrase, setPhrase] = useState({});
+
+  const queryAPI = async () => {
+    const api = await fetch(
+      'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
+    );
+    const phrase = await api.json();
+    setPhrase(phrase[0]);
+  };
+
+  useEffect(() => {
+    queryAPI();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Phrase phrase={phrase} />
+      <Button onClick={queryAPI}>Click me</Button>
+    </Container>
   );
 }
 
